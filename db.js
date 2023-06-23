e14e6abc684f31b30b577521052e3166a3c1cc06
devarshi009 <devarshiojha009@gmail.com>
2023-06-23 18:59:53 +0530

    backend

--- db.js
+++ db.js
@@ -1,4 +1,7 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-const connection=mongoose.connect("mongodb+srv://goldstone:goldstone@cluster0.xcqiqny.mongodb.net/goldstone?retryWrites=true&w=majority")
-module.exports={connection}
+
+const connection = mongoose.connect(process.env.mongooseURL);
+
+module.export = { connection };
